15337d5c2df5ff002cba8c1fbd67447ae1ab4a69
Himavarshith Reddy <himavarshithreddy@gmail.com>
2025-12-14 23:47:38 +0530

    Update api.js

--- apps/web/src/lib/api.js
+++ apps/web/src/lib/api.js
@@ -1,4 +1,4 @@
-const API_BASE = process.env.NEXT_PUBLIC_API_BASE || "http://localhost:4000";
+const API_BASE = process.env.NEXT_PUBLIC_API_BASE || "/api"
 
 export async function apiFetch(path, options = {}) {
   const res = await fetch(`${API_BASE}${path}`, {
